427bc041f1fa1681db720883b79ad59b05683a54
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2023-10-19 16:27:06 -0300

    Piano em fase de finalização

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,20 +1,42 @@
 const pianokeys = document.querySelectorAll('.piano-keys .key');
 let audio = new Audio('src/tunes/a.wav')
+let volume = document.querySelector('.volume-slider input')
+let keysCheck = document.querySelector('.keys-check input')
+let mapedKey = [];
+
 
 const playtune = (key) => {
     audio.src = `src/tunes/${key}.wav`
     audio.play()
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {clickedKey.classList.remove('active')}, 150)
 };
 
 pianokeys.forEach((key) => {
     // a propriedade dataset pega os valores contidos na variavel
-    console.log(key.dataset.key)
-    key.addEventListener('click', () => {
-        playtune(key.dataset.key)
-    })
+    key.addEventListener('click', () => playtune(key.dataset.key))
+    mapedKey.push(key.dataset.key);
 })
 
 document.addEventListener('keydown', (e) => {
-    playtune(e.key)
-    console.log(e.key)
+    if (mapedKey.includes(e.key)) {
+        playtune(e.key)
+    }
 })
+
+let handleVolume = (e) => {
+    audio.volume = e.target.value
+}
+
+volume.addEventListener('input', handleVolume)
+
+// keysCheck.addEventListener('click', showHideKeys)
+//     let pianoKeysSpan = document.querySelectorAll('.piano-keys .key .texto-teclas')
+//     console.log(pianoKeysSpan)
+//     if (e.target.checked === true) {
+//         pianoKeysSpan.classList.add('hide')
+//     } else {
+//         pianoKeysSpan.classList.remove('hide')
+//     }
+
